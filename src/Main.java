36daa2ab4ebb8861a704212cf78c38a17ce8b6fb
SergeySkosyrskiy <s-s-e_96@mail.ru>
2023-12-24 17:47:20 +0800

    Initial commit Add pull request

--- src/Main.java
+++ src/Main.java
@@ -74,4 +74,5 @@ quantityStaff=quantityStaff+94;
 quantityHours=quantityStaff*8;
         System.out.println("Если в компании работает - " +quantityStaff+ " человек то всего " +quantityHours+  " часов работы может быть поделено между сотрудниками " );
     }
+
     }
