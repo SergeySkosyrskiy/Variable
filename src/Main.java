ec080d58ad1e9c58b99058dc6b5c031176dc86c1
SergeySkosyrskiy <s-s-e_96@mail.ru>
2023-12-24 15:50:36 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,77 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача№1");
+var dog = 8.0;
+System.out.println("dog="+dog);
+var cat = 3.6;
+System.out.println("cat="+cat);
+var paper = 763789;
+System.out.println("paper="+paper);
+
+
+System.out.println("Задача№2");
+        dog= dog+4;
+        System.out.println("dog="+dog);
+        cat= cat+4;
+        System.out.println("cat="+cat);
+        paper= paper+4;
+        System.out.println("paper="+paper);
+
+System.out.println("Задача№3");
+        dog= dog-3.5;
+        System.out.println("dog="+dog);
+        cat= cat-1.6;
+        System.out.println("cat="+cat);
+        paper= paper-7639;
+        System.out.println("paper="+paper);
+
+System.out.println("Задача№4");
+var friend=19;
+        System.out.println("friend="+friend);
+friend= friend+2;
+        System.out.println("friend="+friend);
+friend= friend/7;
+        System.out.println("friend="+friend);
+
+System.out.println("Задача№5");
+var frog=3.5;
+        System.out.println("frog="+frog);
+frog=frog*10;
+        System.out.println("frog="+frog);
+frog=frog/3.5;
+        System.out.println("frog="+frog);
+frog=frog+4;
+        System.out.println("frog="+frog);
+
+System.out.println("Задача№6");
+var boxer1=78.2;
+var boxer2=82.7;
+        System.out.println("The boxer 1, weight= " +boxer1+ "kg" );
+        System.out.println("The boxer 2, weight= " +boxer2+ "kg");
+var summWeight= boxer1+ boxer2;
+        System.out.println("Weight two boxers= "+summWeight+ "kg");
+var difference = boxer2-boxer1;
+        System.out.println("The difference in the weight of the boxers is= "+difference+ "kg" );
+
+
+
+System.out.println("Задача№7");
+difference = boxer2-boxer1;
+        System.out.println("Разница масс первым способом = "+difference+ "kg" );
+difference = boxer2 % boxer1;
+        System.out.println("Разница масс вторым способом = "+difference+ "kg" );
+
+System.out.println("Задача№8");
+var quantityHours=640;
+var oneEmployee=8;
+var quantityStaff =quantityHours/oneEmployee;
+
+        System.out.println("Всего работников в компании — " +quantityStaff+ " человек" );
+quantityStaff=quantityStaff+94;
+quantityHours=quantityStaff*8;
+        System.out.println("Если в компании работает - " +quantityStaff+ " человек то всего " +quantityHours+  " часов работы может быть поделено между сотрудниками " );
+    }
+    }
